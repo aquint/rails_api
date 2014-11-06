5690de4bc36b9f61c68c630c75226f0e40c8bda8
aquint <aquinton_22@hotmail.com>
2014-11-06 18:33:09 -0500

    ad spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+require 'spec_helper'
 
+describe User do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+  it { should be_valid }
+end
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_not be_valid }
+end
 RSpec.describe User, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
