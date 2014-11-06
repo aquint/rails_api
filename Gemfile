c2df020ce839f06ae0e8ec76a2c952eb04d9a5c9
aquint <aquinton_22@hotmail.com>
2014-11-06 18:01:01 -0500

    versioning

--- Gemfile
+++ Gemfile
@@ -28,6 +28,12 @@ group :development do
   gem 'sqlite3'
 end
 
+group :test do
+  gem "rspec-rails"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+require 'api_contraints'
+
 MarketPlaceApi::Application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end                
   end
 end
