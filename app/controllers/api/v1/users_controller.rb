64f9fa343ec3ea93933dd81514f0ee87b0c66af7
aquint <aquinton_22@hotmail.com>
2014-11-07 18:33:29 -0500

    add authenticate

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,13 +13,13 @@ class Api::V1::UsersController < ApplicationController
     end
   end
   def update
-	user = User.find(params[:id])
+  	user = current_user
 
-	if user.update(user_params)
-	  render json: user, status: 200, location: [:api, user]
-	else
-	  render json: { errors: user.errors }, status: 422
-	end
+      if user.update(user_params)
+        render json: user, status: 200, location: [:api, user]
+      else
+        render json: { errors: user.errors }, status: 422
+      end
   end
   def destroy
     user = User.find(params[:id])
